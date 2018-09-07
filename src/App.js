23fe049ef0aa50286d168996aefd3881657b5c5f
Kevin <nait90@gmail.com>
2018-09-07 16:17:25 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+import MovieBrowser from './modules/movie-browser/movie-browser.container'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+class App extends Component {
+  render() {
+    return (
+        <MuiThemeProvider>
+            <MovieBrowser />
+        </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/modules/movie-browser/movie-browser.container.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Grid, Row, Col} from 'react-bootstrap'
+import {AppBar} from 'material-ui'
+
+import * as movieActions from './movie-browser.actions'
+import * as movieHelpers from './movie-browser.helpers'
+import MovieList from './movie-list/movie-list.component'
+import * as scrollHelpers from '../common/scroll.helpers'
+import MovieModal from './movie-modal/movie-modal.container'
+
+class MovieBrowser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 1
+        };
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+    componentDidMount() {
+        window.onscroll = this.handleScroll;
+        this.props.getTopMovies(this.state.currentPage);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+        const {topMovies} = this.props;
+
+        if (!topMovies.isLoading) {
+            let percentageScrolled = scrollHelpers.getScrollDownPercentage(window);
+            if (percentageScrolled > .9) {
+                const nextPage = this.state.currentPage + 1;
+                this.props.getTopMovies(nextPage);
+                this.setState({currentPage: nextPage});
+            }
+        }
+    }
+
+    render() {
+        const {topMovies} = this.props;
+        const movies = movieHelpers.getMoviesList(topMovies.response);
+
+        return (
+            <div>
+                <AppBar title='Movie Browser' />
+                <Grid>
+                    <Row>
+                        <p>Search will go here</p>
+                    </Row>
+                    <Row>
+                        <MovieList movies={movies} isLoading={topMovies.isLoading} />
+                    </Row>
+                </Grid>
+                {/* <MovieModal /> */}
+            </div>
+        );
+    }
+}
+
+export default connect(
+    //mapStateToProps()
+    //Map nodes in our state to a properties of our component
+    (state) => ({
+        topMovies: state.movieBrowser.topMovies
+    }),
+    //mapDispatchToProps()
+    // Map action creators to properties of our component
+    { ...movieActions }
+)(MovieBrowser);
